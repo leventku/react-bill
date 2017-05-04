de04c161a0b8149fa9e51f69036342d6d19b13b6
Levent Kucukgorkey <levent.kucukgorkey@gmail.com>
2017-05-04 21:33:00 +0100

    added readme

---
+++ PackageCharges.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import PackageCharges from '../src/PackageCharges';
+
+function setupComponent() {
+  const props = {
+    data: {
+      "subscriptions": [
+        { "type": "tv", "name": "Variety with Movies HD", "cost": 50.00 }
+      ],
+    }
+  }
+
+  const wrapper = shallow(<PackageCharges {...props} />)
+
+  return  {
+    props,
+    wrapper
+  }
+}
+
+describe('PackageCharges component', () => {
+  it('should render', () => {
+    const {wrapper} = setupComponent();
+    expect(wrapper.exists()).toBe(true);
+  })
+
+  it('should render its items', () => {
+    const {wrapper} = setupComponent();
+    expect(toJson(wrapper)).toMatchSnapshot();
+  })
+})
---
+++ README.md
@@ -0,0 +1,14 @@
+# Getting Started
+
+```
+	> npm install
+	> npm start
+```
+
+navigate to `http://localhost:8080/`
+
+## Running tests
+
+```
+	> npm test
+```
